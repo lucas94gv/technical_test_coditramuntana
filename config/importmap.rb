690b9835bfa415c732521cc90d53f820877cdab8
Lucas <lucas94gv@gmail.com>
2024-08-29 23:13:10 +0200

    add menu and bootstrap configuration

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+
+# config/importmap.rb
+pin 'bootstrap', to: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', preload: true
+pin 'bootstrap-icons', to: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css', preload: true
