a63d87641b8f9af4764a42cc90138c2cf61230cc
Lucas <lucas94gv@gmail.com>
2024-08-29 13:41:44 +0200

    add initial tests for artists

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -2,31 +2,41 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Artists', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/artists/index'
+RSpec.describe ArtistsController, type: :controller do
+  before(:each) do
+    @artist1 = FactoryBot.create(:artist)
+    @artist2 = FactoryBot.create(:artist)
+    @artist3 = FactoryBot.create(:artist)
+  end
+
+  describe 'Get #index' do
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'total artists is 3' do
+      expect(Artist.all.count).to eq(3)
+    end
+  end
+end
+
+RSpec.describe ArtistsController, type: :controller do
+  describe 'Get #show' do
+    it 'returns a successful response' do
+      get :show, params: { id: @artist1.id }
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/artists/show'
+  describe 'GET #new' do
+    it 'returns a successful response' do
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/artists/new'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /edit' do
-    it 'returns http success' do
-      get '/artists/edit'
+  describe 'GET #edit' do
+    it 'returns a successful response' do
+      get :edit, params: { id: @artist1.id }
       expect(response).to have_http_status(:success)
     end
   end
