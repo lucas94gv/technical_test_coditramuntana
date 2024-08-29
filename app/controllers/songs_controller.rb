164401ed1736c867ac67b68d213086f365f2bf46
Lucas <lucas94gv@gmail.com>
2024-08-30 00:17:07 +0200

    add songs controller

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+# Controller to manage operations related to songs.
+class SongsController < ApplicationController
+  before_action :find_song, only: %i[show edit update destroy]
+  def index
+    @songs = Song.all
+  end
+
+  def show; end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    respond_to do |format|
+      if @song.save
+        format.html { redirect_to song_url(@song), notice: 'Song was successfully created.' }
+        format.json { render :show, status: :created, location: @song }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @song.update(song_params)
+        format.html { redirect_to lp_url(@song), notice: 'Song was successfully updated.' }
+        format.json { render :show, status: :ok, location: @song }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @song.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to songs_url, notice: 'Song was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :lp_id)
+  end
+end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# This module provides helper methods for views related to the Song model.
 module SongsHelper
 end
