e707bcc168e4a828b36fd7dfde154c3b31472560
Lucas <lucas94gv@gmail.com>
2024-08-30 00:36:04 +0200

    add tests for authors requests

---
+++ spec/requests/authors_spec.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AuthorsController, type: :controller do
+  before(:each) do
+    @author = FactoryBot.create(:author)
+  end
+
+  describe 'Get #index' do
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
+
+RSpec.describe AuthorsController, type: :controller do
+  before(:each) do
+    @author = FactoryBot.create(:author)
+  end
+
+  describe 'Get #show' do
+    it 'returns a successful response' do
+      get :show, params: { id: @author.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'Get #new' do
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'Get #edit' do
+    it 'returns a successful response' do
+      get :edit, params: { id: @author.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
+
+RSpec.describe AuthorsController, type: :controller do
+  describe 'POST #create' do
+    it 'creates a new Author with valid parameters' do
+      expect do
+        post :create, params: { author: { name: Faker::Name.first_name } }
+      end.to change(Author, :count).by(1)
+    end
+
+    it 'not create new Author with invalid parameters' do
+      expect do
+        post :create, params: { author: { name: nil, description: nil } }
+      end.not_to change(Author, :count)
+    end
+  end
+end
+
+RSpec.describe AuthorsController, type: :controller do
+  before(:each) do
+    @author = FactoryBot.create(:author, name: 'Lucas')
+  end
+
+  describe 'PATCH #update' do
+    it 'updates the requested author with valid parameters' do
+      patch :update, params: { id: @author.id, author: { name: 'Simon' } }
+      @author.reload
+      expect(@author.name).to eq('Simon')
+    end
+
+    it 'redirects to the author' do
+      patch :update, params: { id: @author.id, author: { name: 'Simon' } }
+      expect(response).to redirect_to(@author)
+    end
+
+    it 'does not update the author with invalid parameteres' do
+      patch :update, params: { id: @author.id, author: { name: nil } }
+      @author.reload
+      expect(@author.name).to eq('Lucas')
+    end
+  end
+end
+
+RSpec.describe AuthorsController, type: :controller do
+  before(:each) do
+    @author = FactoryBot.create(:author, name: 'Anton')
+  end
+
+  describe 'PATCH #update' do
+    it 'deletes the requested author' do
+      expect do
+        delete :destroy, params: { id: @author.id }
+      end.to change(Author, :count).by(-1)
+    end
+  end
+end
