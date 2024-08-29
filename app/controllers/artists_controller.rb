02a5262e34408d773a317bbc4497da5f379f50f3
Lucas <lucas94gv@gmail.com>
2024-08-29 15:52:05 +0200

    add basic create update and destroy

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,7 +2,7 @@
 
 # Controller to manage operations related to artists.
 class ArtistsController < ApplicationController
-  before_action :find_artist, only: %i[show edit]
+  before_action :find_artist, only: %i[show edit update destroy]
   def index
     @artists = Artist.all
   end
@@ -13,11 +13,50 @@ class ArtistsController < ApplicationController
     @artist = Artist.new
   end
 
+  def create
+    @artist = Artist.new(artist_params)
+
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to artist_url(@artist), notice: 'Artist was successfully created.' }
+        format.json { render :show, status: :created, location: @artist }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def edit; end
+
+  def update
+    respond_to do |format|
+      if @artist.update(artist_params)
+        format.html { redirect_to artist_url(@artist), notice: 'Artist was successfully updated.' }
+        format.json { render :show, status: :ok, location: @artist }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @artist.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to artists_url, notice: 'Artist was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
 
   def find_artist
     @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :description)
   end
 end
