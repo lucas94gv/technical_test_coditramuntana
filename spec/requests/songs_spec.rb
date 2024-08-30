1adb7e605f0a6fcba9c6299bb65aa8551ea2740a
Lucas <lucas94gv@gmail.com>
2024-08-30 17:22:11 +0200

    fix tests due to nested routes

--- spec/requests/songs_spec.rb
+++ spec/requests/songs_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe SongsController, type: :controller do
 
   describe 'Get #show' do
     it 'returns a successful response' do
-      get :show, params: { id: @song.id }
+      get :show, params: { lp_id: @song.lp_id, id: @song.id }
       expect(response).to have_http_status(:success)
     end
   end
@@ -34,7 +34,7 @@ RSpec.describe SongsController, type: :controller do
 
   describe 'Get #edit' do
     it 'returns a successful response' do
-      get :edit, params: { id: @song.id }
+      get :edit, params: { lp_id: @song.lp_id, id: @song.id }
       expect(response).to have_http_status(:success)
     end
   end
@@ -49,13 +49,13 @@ RSpec.describe SongsController, type: :controller do
     it 'creates a new song with valid parameters' do
       expect do
         post :create,
-             params: { song: { name: Faker::Name.first_name, lp_id: @lp.id } }
+             params: { lp_id: @lp.id, song: { name: Faker::Name.first_name, lp_id: @lp.id } }
       end.to change(Song, :count).by(1)
     end
 
     it 'not create new song with invalid parameters' do
       expect do
-        post :create, params: { song: { name: nil } }
+        post :create, params: { lp_id: @lp.id, song: { name: nil } }
       end.not_to change(Song, :count)
     end
   end
@@ -69,13 +69,13 @@ RSpec.describe SongsController, type: :controller do
 
   describe 'PATCH #update' do
     it 'updates the requested song with valid parameters' do
-      patch :update, params: { id: @song.id, song: { name: 'Woke Up This Morning' } }
+      patch :update, params: { lp_id: @song.lp_id, id: @song.id, song: { name: 'Woke Up This Morning' } }
       @song.reload
       expect(@song.name).to eq('Woke Up This Morning')
     end
 
     it 'does not update the lp with invalid parameteres' do
-      patch :update, params: { id: @song.id, song: { name: nil } }
+      patch :update, params: { lp_id: @song.lp_id, id: @song.id, song: { name: nil } }
       @song.reload
       expect(@song.name).to eq('Dont Stop Believing')
     end
@@ -90,7 +90,7 @@ RSpec.describe SongsController, type: :controller do
   describe 'PATCH #update' do
     it 'deletes the requested song' do
       expect do
-        delete :destroy, params: { id: @song.id }
+        delete :destroy, params: { lp_id: @song.lp_id, id: @song.id }
       end.to change(Song, :count).by(-1)
     end
   end
